02729a50a002c78f18e752da5ceb1b80f4488b7f
Jakub Samotyja <jjsamotyja@gmail.com>
2023-11-16 17:14:08 +0100

    add first working implementation of 'say' command and small bugfixes

--- commands/Minecraft/say.js
+++ commands/Minecraft/say.js
@@ -1,4 +1,12 @@
+const util = require('mc-server-utilities');
 const { SlashCommandBuilder } = require('discord.js');
+const { rconPort, serverIp, rconPassword } = require('../../server-config.json');
+
+const client = new util.RCON();
+
+const options = {
+	timeout: 1000 * 5,
+};
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -11,9 +19,15 @@ module.exports = {
 				.setRequired(true)),
 
 	async execute(interaction) {
-		const msg = interaction.options.getString('wiadomość') ?? 'No reason provided';
+		console.log(`command '${interaction.commandName}' awakened  by ${interaction.user.globalName}`);
+		const msg = interaction.options.getString('wiadomość') ?? 'Nie podano wiadomości';
+		await client.connect(serverIp, rconPort, options);
+		await client.login(rconPassword);
+		const message = await client.execute(`/say ${interaction.user.globalName}: ${msg}`);
+		console.log(message);
+		await client.close();
+		await interaction.reply({ content: `Wiadomość o treści "${msg}" została wysłana na serwer`, ephemeral: true });
 
-		await interaction.reply(`${msg}`);
 	},
 
 };
